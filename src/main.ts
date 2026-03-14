fb19891f546d1ffa561866ce8f70ceef9d434609
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2026-03-13 22:43:59 -0300

    usando db

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
 import crypo from "crypto";
+import pgp from "pg-promise";
 const app = express();
 
+const connection = pgp()("postgres://postgres:123456@db/trade_db");
+console.log("Database connection established", connection);
+
 app.post("/signup", (req: Request, res: Response) => {
-  console.log("Signup endpoint hit2");
+  const account = req.body;
+  console.log("Signup endpoint hit with account data:", account);
   const accountId = crypo.randomUUID();
+  res.json({ accountId });
+});
+
+app.get("/accounts/:accountId", async (req: Request, res: Response) => {
+  const accountId = req.params.accountId;
+  console.log(`Get account endpoint hit for accountId: ${accountId}`);
+  const [account] = await connection.query("SELECT * FROM trade.account", []);
   res.json({ accountId });
 });
 
--- test/main.test.ts
+++ test/main.test.ts
@@ -16,5 +16,11 @@ describe("", () => {
 
     //Then
     expect(outputSignup.accountId).toBeDefined();
+    const responseGetAccount = await axios.get(`http://localhost:3000/accounts/${outputSignup.accountId}`);
+    const outputGetAccount = responseGetAccount.data;
+    expect(outputGetAccount.name).toBe(input.name);
+    expect(outputGetAccount.email).toBe(input.email);
+    expect(outputGetAccount.document).toBe(input.document);
+    expect(outputGetAccount.password).toBe(input.password);
   });
 });
